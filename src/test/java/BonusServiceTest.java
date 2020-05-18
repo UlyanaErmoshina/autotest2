cf3bf19386c701acd8e47617d9faf8936e66ec02
Ulyana Ermoshina <uermoshina@inbox.ru>
2020-05-18 13:15:54 +0300

    Inn commit

--- src/test/java/BonusServiceTest.java
+++ src/test/java/BonusServiceTest.java
@@ -10,7 +10,7 @@ class BonusServiceTest {
         boolean registered = true;
         long expected = 30;
 
-        long actual = service.calculate(amount,registered);
+        long actual = service.calculate(amount, registered);
 
         assertEquals(expected, actual);
     }
@@ -23,19 +23,33 @@ class BonusServiceTest {
         boolean registered = true;
         long expected = 500;
 
-        long actual = service.calculate(amount,registered);
+        long actual = service.calculate(amount, registered);
 
         assertEquals(expected, actual);
     }
+
     @org.junit.jupiter.api.Test
-    void shouldCalculateForNotRegisteredAndUnderLimit() {
+    void shouldCalculateForNotRegistered() {
         BonusService service = new BonusService();
 
         long amount = 1000_60;
         boolean registered = false;
+        long expected = 10;
+
+        long actual = service.calculate(amount, registered);
+
+        assertEquals(expected, actual);
+    }
+
+    @org.junit.jupiter.api.Test
+    void shouldCalculateForRegistered() {
+        BonusService service = new BonusService();
+
+        long amount = 1000_60;
+        boolean registered = true;
         long expected = 30;
 
-        long actual = service.calculate(amount,registered);
+        long actual = service.calculate(amount, registered);
 
         assertEquals(expected, actual);
     }
