d6e54921adc82582c8bd4f2311b7d21b90ebf96a
Ulyana Ermoshina <uermoshina@inbox.ru>
2020-05-18 14:17:03 +0300

    Innit commit

--- src/test/java/BonusServiceTest.java
+++ src/test/java/BonusServiceTest.java
@@ -29,7 +29,7 @@ class BonusServiceTest {
     }
 
     @org.junit.jupiter.api.Test
-    void shouldCalculateForNotRegistered() {
+    void shouldCalculateForNotRegisteredUnderLimit() {
         BonusService service = new BonusService();
 
         long amount = 1000_60;
@@ -42,12 +42,12 @@ class BonusServiceTest {
     }
 
     @org.junit.jupiter.api.Test
-    void shouldCalculateForRegistered() {
+    void shouldCalculateForNotRegisteredOverLimit() {
         BonusService service = new BonusService();
 
-        long amount = 1000_60;
-        boolean registered = true;
-        long expected = 30;
+        long amount = 1_000_000_60;
+        boolean registered = false;
+        long expected = 500;
 
         long actual = service.calculate(amount, registered);
 
